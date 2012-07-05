6d114cd688421c59875035dbc86957aee0902bb3
Oliver Fields <oliver@phnd.net>
2012-07-05 09:36:26 +0200

    Unittests are good

---
+++ requirements/unittests/Requirement_test.py
@@ -0,0 +1,99 @@
+from ..Requirement import *
+import pytest
+
+class TestRequirement():
+	'''Unit tests'''
+
+	def test_valid_req_config(self):
+		req = Requirement()
+		req.load_config_from_file('unittests/test_data/requirement_config.req')
+
+		assert req.assigned_on == '2012-07-04'
+		assert req.assigned_to == 'testclient'
+		assert req.created_by == 'testuser'
+		assert req.created_on == '2012-07-04'
+		assert req.description == 'Nice requirement for testing with'
+		assert req.depends_on == 'none'
+		assert req.documents == 'none'
+		assert req.estimated_effort == '400'
+		assert req.estimated_cost == '15000'
+		assert req.note == 'A quick note'
+		assert req.rationale == 'The why is paramount'
+		assert req.rejected_by == 'testclient2'
+		assert req.status == 'approved'
+		assert req.status_reason == 'Not really'
+		assert req.todo == '- Write tests'
+
+	def test_valid_req_config_1(self):
+		req = Requirement()
+		req.load_config_from_file('unittests/test_data/requirement_config_1.req')
+
+		assert req.description == 'Nice requirement for testing with'
+		assert req.rationale == 'The why is paramount'
+
+	def test_valid_req_config_2(self):
+		req = Requirement()
+		req.load_config_from_file('unittests/test_data/requirement_config_2.req')
+
+		assert req.assigned_on == '2012-07-04'
+		assert req.assigned_to == 'testclient'
+		assert req.description == 'Nice requirement for testing with\nAnd more text # this is not a comment'
+		assert req.depends_on == 'none'
+		assert req.estimated_effort == '400'
+		assert req.rationale == 'The why is paramount # this is not a comment'
+		assert req.status == 'approved'
+		assert req.status_reason == 'Not really'
+
+	def test_valid_req_config_3(self):
+		req = Requirement()
+		req.load_config_from_file('unittests/test_data/requirement_config_3.req')
+
+		assert req.description == 'Nice requirement for testing with'
+		assert req.rationale == 'The why is paramount\nThe why is paramount2'
+		assert req.status == 'elaboration'
+		assert req.todo == '- test multi lines'
+
+	def test_valid_req_config_4(self):
+		req = Requirement()
+		req.load_config_from_file('unittests/test_data/requirement_config_4.req')
+
+		assert req.assigned_on == '2012-07-04 is a bad date'
+		assert req.assigned_to == 'testclientnot is not a client'
+		assert req.description == 'Nice requirement for testing with'
+		assert req.depends_on == 'none nothing is not allowed setting'
+		assert req.documents == 'none'
+		assert req.estimated_effort == '4.00'
+		assert req.estimated_cost == '15, 000'
+		assert req.note == 'A quick note'
+		assert req.rationale == 'The why is paramount'
+		assert req.status == 'elaboration'
+
+	def test_file_missing_extension(self):
+		req = Requirement()
+		with pytest.raises(SystemExit):
+			req.load_config_from_file('unittests/test_data/file_missing_extension')
+
+	def test_invalid_req_config(self):
+		req = Requirement()
+		with pytest.raises(SystemExit):
+			req.load_config_from_file('unittests/test_data/invalid_requirement_config.req')
+
+	def test_invalid_req_config_1(self):
+		req = Requirement()
+		with pytest.raises(SystemExit):
+			req.load_config_from_file('unittests/test_data/invalid_requirement_config_1.req')
+
+	def test_invalid_req_config_2(self):
+		req = Requirement()
+		with pytest.raises(SystemExit):
+			req.load_config_from_file('unittests/test_data/invalid_requirement_config_2.req')
+
+	def test_invalid_req_config_3(self):
+		req = Requirement()
+		with pytest.raises(SystemExit):
+			req.load_config_from_file('unittests/test_data/invalid_requirement_config_3.req')
+
+	def test_invalid_req_config_4(self):
+		req = Requirement()
+		with pytest.raises(SystemExit):
+			req.load_config_from_file('unittests/test_data/invalid_requirement_config_4.req')
