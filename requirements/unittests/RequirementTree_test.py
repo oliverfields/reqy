4f50eec571894deedf5999200c5361e8ba3dae3d
Oliver Fields <oliver@phnd.net>
2012-07-09 19:25:58 +0200

    Unittests for the tree

---
+++ requirements/unittests/RequirementTree_test.py
@@ -0,0 +1,10 @@
+from ..RequirementTree import *
+import pytest
+
+class TestRequirementTree():
+	'''Unit tests'''
+
+	def test_sample_repo_load(self):
+		tree = RequirementTree()
+		tree.load_repository('unittests/test_data/sample-repository')
+
