0959c7bd8447a9d8d5cd47c42b0d5f5b02ecb7ef
Oliver Fields <oliver@phnd.net>
2012-07-09 23:35:55 +0200

    Changed doc file name and path fields to standard

--- requirements/unittests/Document_test.py
+++ requirements/unittests/Document_test.py
@@ -7,11 +7,11 @@ class TestDocument():
 	def test_document_loads(self):
 		doc = Document()
 		doc.load_document('unittests/test_data/sample-repository/documents/design/erd.png')
-		assert doc.name == 'erd.png'
+		assert doc._file_name == 'erd.png'
 		# The path should be absolute, so in order to make the unit tests
 		# portable it is hard to check what it should be, will just have to do
 		# to know it isn't empty..
-		assert doc.path != ''
+		assert doc._file_path != ''
 
 	def test_non_existent_document(self):
 		doc = Document()
